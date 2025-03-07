c05b0b532897bec015a39f7287d1dcaa2b6e8a74
CangShuV <2862470532@qq.com>
2025-03-07 16:17:21 +0800

    Update test.c

--- run/test.c
+++ run/test.c
@@ -1,3 +1,15 @@
+int* mpi_receive(int* x, int val){
+    return x;
+}
 int main(){
+int a = 0;
+int buf = 0;
+for(int i = 0; i < a; i++){
+int *p = &buf;
+*p = 1;
+mpi_receive(p, 1);
+int *q = &buf;
+printf("%d", *q);
+}
 return 0;
 }
