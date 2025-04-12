13263fbada27cf59a8a0381f992ace568c68a1ba
CangShuV <2862470532@qq.com>
2025-04-12 20:50:58 +0800

    Update DT Makefile

--- run/NPB3.4.3/NPB3.4-MPI/DT/npbparams.h
+++ run/NPB3.4.3/NPB3.4-MPI/DT/npbparams.h
@@ -7,11 +7,11 @@
 #define NUM_SAMPLES 110592
 #define STD_DEVIATION 512
 #define NUM_SOURCES 16
-#define COMPILETIME "10 Apr 2025"
+#define COMPILETIME "12 Apr 2025"
 #define NPBVERSION "3.4.3"
 #define MPICC "mpicc"
 #define CFLAGS "-O3"
 #define CLINK "$(MPICC)"
 #define CLINKFLAGS "$(CFLAGS)"
-#define CMPI_LIB "(none)"
-#define CMPI_INC "(none)"
+#define CMPI_LIB "$(if $(MPI_LIB),-L$(MPI_LIB)) -lmpi"
+#define CMPI_INC "$(if $(MPI_INC),-I$(MPI_INC))"
